affcfca50ce9a1663b2fe2eeac9b8847d616f67c
nico <nroman@eclass.cl>
2020-05-29 16:56:59 -0400

    feat(package) static files

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  IMAGE_URL: process.env.NODE_ENV === 'production' ? '' : 'https://catacepedallanos.com/img/',
-  ICON_URL: process.env.NODE_ENV === 'production' ? '' : 'https://catacepedallanos.com/Icons/'
+  IMAGE_URL: process.env.NODE_ENV === 'production' ? '' : '/img/',
+  ICON_URL: process.env.NODE_ENV === 'production' ? '' : '/Icons/'
 }
